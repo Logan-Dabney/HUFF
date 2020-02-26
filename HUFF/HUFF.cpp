cc97efcd214bcd210107399863e4e9c1250bf77a
Logan Dabney <logan.dabney@rockets.utoledo.edu>
2020-02-26 11:54:16 -0500

    Add project files.

---
+++ HUFF/HUFF.cpp
@@ -0,0 +1,13 @@
+// HUFF.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	// open input file stream 
+	// use get method to read one char at a time
+
+}
